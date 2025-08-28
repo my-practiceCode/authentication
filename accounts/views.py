85292f095b9bee61dd464bf301335356b214fbd2
my-practice <hafizrehamanees786@gmail.com>
2025-08-28 12:48:13 +0500

    "Authentication system in django"

---
+++ accounts/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render , redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login , logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+
+def home(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        email = User.objects.normalize_email(email)
+
+        
+        if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username already exists')
+                return redirect('home')
+            
+        if  User.objects.filter(email=email).exists():
+                messages.error(request, 'email already exists')
+                return redirect('home')
+            
+        User.objects.create_user(username=username, email=email, password=password)
+        messages.success(request, 'Account created successfully! You can now log in.')
+        return redirect('login')
+        
+            
+    return render(request,'index.html')
+
+def loginPage(request):
+    user = User.objects.all()
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:   # authentication successful
+            login(request, user)
+            return redirect('/about/')
+        else:   # authentication failed
+            messages.error(request, 'Invalid username or password')
+            return redirect('login')  # redirect back to login page
+        
+    return render(request, 'login.html')
+
+
+def logoutPage(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
+def about(request):
+    return render(request,'about.html')
